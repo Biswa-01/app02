3091b5bf44a078b816578d68b8d0ef188c970e2b
biswa123 <rbiswa035@gmail.com>
2024-10-07 16:06:21 +0530

    4th commit

---
+++ src/test/java/com/axicTech/biswa/SampleTest.java
@@ -0,0 +1,20 @@
+package com.axicTech.biswa;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SampleTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+	System.out.println("Testing Passed");
+    }
+}
